ac061359f1a6cccdfa4b672be6712199ea238e05
Rap Mendoza <mendozarap@gmail.com>
2020-07-22 17:21:14 +0800

    Integrate bulma

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,17 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
 
-export default props => <div>Minimal App</div>;
+export default props => {
+  return (
+    <section class="hero is-primary is-fullheight is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Hey there! Welcome to blogarap.</h1>
+          <button class="button is-primary is-inverted is-outlined">
+            Get started.
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
