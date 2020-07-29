1a64aa3e8fb12adb183ac20d66d4b9d55d31c86e
Rap Mendoza <mendozarap@gmail.com>
2020-07-29 19:15:36 +0800

    Use hooks in blog details

--- src/Components/Blogs/Blog/blog.js
+++ src/Components/Blogs/Blog/blog.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Nav from '../../Nav';
 
-export default class extends Component {
-  state = {
-    isLoading: true,
-    blog: {
+export default props => {
+  const [isLoading, setIsLoading] = useState(false),
+    [blog, setBlog] = useState({
       title: '',
       content: '',
       author: '',
-    },
-  };
+    });
 
-  componentDidMount() {
-    this.getBlog(this.props.match.params.id);
-  }
+  useEffect(() => {
+    getBlog(props.match.params.id);
+  }, []);
 
-  getBlog(id) {
+  const getBlog = id => {
+    setIsLoading(true);
+
     fetch(`https://blogarap-api.herokuapp.com/blogs/${id}`, {
       method: 'GET',
       headers: {
@@ -27,64 +27,59 @@ export default class extends Component {
       .then(response => response.json())
       .then(b => {
         setTimeout(() => {
-          this.setState({
-            isLoading: false,
-            blog: {
-              title: b.title,
-              content: b.content,
-              author: b.author.name,
-            },
+          setIsLoading(false);
+          setBlog({
+            title: b.title,
+            content: b.content,
+            author: b.author.name,
           });
         }, 1500);
       });
-  }
+  };
 
-  render() {
-    const { isLoading, blog } = this.state;
-
-    return (
-      <div>
-        <div className="hero is-dark is-fullheight is-bold">
-          <Nav />
-          <div className="hero-body">
-            <div className="container">
-              {isLoading ? (
-                <progress className="progress is-small is-primary" max="100">
-                  100%
-                </progress>
-              ) : (
-                <section className="section">
-                  <div className="columns is-mobile">
-                    <div className="column is-three-fifths-desktop is-offset-one-fifth-desktop">
-                      <h1 className="title">{blog.title}</h1>
-                      <h2 className="subtitle has-text-grey-light is-italic is-size-6 is-capitalized">
-                        by{' '}
-                        <span className="has-text-weight-semibold">
-                          {blog.author}
-                        </span>
-                      </h2>
-                      <p
-                        style={{
-                          whiteSpace: 'pre-wrap',
-                          letterSpacing: '.03rem',
-                        }}
-                      >
-                        {blog.content}
-                      </p>
-                      <Link
-                        to="/blogs"
-                        className="button is-light is-outlined mt-6"
-                      >
-                        Back
-                      </Link>
-                    </div>
+  return (
+    <div>
+      <div className="hero is-dark is-fullheight is-bold">
+        <Nav />
+        <div className="hero-body">
+          <div className="container">
+            {!isLoading && (
+              <section className="section">
+                <div className="columns is-mobile">
+                  <div className="column is-three-fifths-desktop is-offset-one-fifth-desktop">
+                    <h1 className="title">{blog.title}</h1>
+                    <h2 className="subtitle has-text-grey-light is-italic is-size-6 is-capitalized">
+                      by{' '}
+                      <span className="has-text-weight-semibold">
+                        {blog.author}
+                      </span>
+                    </h2>
+                    <p
+                      style={{
+                        whiteSpace: 'pre-wrap',
+                        letterSpacing: '.03rem',
+                      }}
+                    >
+                      {blog.content}
+                    </p>
+                    <Link
+                      to="/blogs"
+                      className="button is-light is-outlined mt-6"
+                    >
+                      Back
+                    </Link>
                   </div>
-                </section>
-              )}
-            </div>
+                </div>
+              </section>
+            )}
           </div>
         </div>
+        {isLoading && (
+          <progress className="progress is-small is-primary" max="100">
+            100%
+          </progress>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
--- src/Components/Blogs/Blog/index.js
+++ src/Components/Blogs/Blog/index.js
@@ -2,85 +2,77 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 
-export default class extends Component {
-  handleMouseEnter(e) {
+export default props => {
+  const handleMouseEnter = e => {
     const article = e.target;
 
     article.classList.remove('is-light');
     article.classList.add('is-white');
-  }
+  };
 
-  handleMouseLeave(e) {
+  const handleMouseLeave = e => {
     const article = e.target;
 
     article.classList.remove('is-white');
     article.classList.add('is-light');
-  }
+  };
 
-  render() {
-    const {
-      blog,
-      handleToggleEdit,
-      handleToggleDelete,
-      isLoggedIn,
-    } = this.props;
-    const canEdit = parseInt(sessionStorage.getItem('id')) === blog.author.id;
+  const { blog, handleToggleEdit, handleToggleDelete, isLoggedIn } = props;
+  const canEdit = parseInt(sessionStorage.getItem('id')) === blog.author.id;
 
-    return (
-      <div className="tile is-ancestor">
-        <div className="tile is-vertical">
-          <div className="tile">
-            <div className="tile is-parent is-vertical">
-              <article
-                className="tile is-child notification is-light"
-                style={{ cursor: 'pointer' }}
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-              >
-                <div className="level is-mobile mb-1">
-                  <h2 className="level-left has-text-black has-text-weight-semibold is-size-3">
-                    {blog.title}
-                  </h2>
+  return (
+    <div className="tile is-ancestor">
+      <div className="tile is-vertical">
+        <div className="tile">
+          <div className="tile is-parent is-vertical">
+            <article
+              className="tile is-child notification is-light"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className="level is-mobile mb-1">
+                <h2 className="level-left has-text-black has-text-weight-semibold is-size-3">
+                  {blog.title}
+                </h2>
 
-                  {isLoggedIn && canEdit && (
-                    <div className="level-right">
-                      <Button color="is-info" toggle={handleToggleEdit}>
-                        Edit
-                      </Button>
-                      <Button color="is-danger" toggle={handleToggleDelete}>
-                        Delete
-                      </Button>
-                    </div>
-                  )}
-                </div>
+                {isLoggedIn && canEdit && (
+                  <div className="level-right">
+                    <Button color="is-info" toggle={handleToggleEdit}>
+                      Edit
+                    </Button>
+                    <Button color="is-danger" toggle={handleToggleDelete}>
+                      Delete
+                    </Button>
+                  </div>
+                )}
+              </div>
 
-                <p className="subtitle has-text-grey is-italic is-size-6 is-capitalized">
-                  by{' '}
-                  <span className="has-text-weight-semibold">
-                    {blog.author.name}
-                  </span>
+              <p className="subtitle has-text-grey is-italic is-size-6 is-capitalized">
+                by{' '}
+                <span className="has-text-weight-semibold">
+                  {blog.author.name}
+                </span>
+              </p>
+
+              <div className="content">
+                <p
+                  style={{
+                    whiteSpace: 'pre-wrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    WebkitLineClamp: 2,
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                >
+                  {blog.content}
                 </p>
-
-                <div className="content">
-                  <p
-                    style={{
-                      whiteSpace: 'pre-wrap',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      display: '-webkit-box',
-                      WebkitLineClamp: 2,
-                      WebkitBoxOrient: 'vertical',
-                    }}
-                  >
-                    {blog.content}
-                  </p>
-                  <Link to={`/blogs/${blog.id}`}>Continue Reading</Link>
-                </div>
-              </article>
-            </div>
+                <Link to={`/blogs/${blog.id}`}>Continue Reading</Link>
+              </div>
+            </article>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
